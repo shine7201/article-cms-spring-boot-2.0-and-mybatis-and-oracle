7e38b07bd2fb7b515eb440f25a51fa03727710e0
shine7201 <shine7201@gmail.com>
2020-04-18 12:24:29 +0900

    ArticleService, ArticleServiceImpl, ArticleDto 생성 및 세팅, getList() 함수를 구현(일단 가짜데이터)

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -1,13 +1,29 @@
 package com.sbs.starter.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class ArticleController {
+import com.sbs.starter.dto.Article;
+import com.sbs.starter.service.ArticleService;
 
+import groovy.util.logging.Slf4j;
+import jline.internal.Log;
+
+@Controller
+@Slf4j
+public class ArticleController {
+	@Autowired
+	ArticleService articleService;
+	
 	@RequestMapping("/article/list")
 	public String showList() {
+		List<Article> list = articleService.getList();
+		
+		Log.info("list : "+list);
+		
 		return "article/list";
 	}
 }
