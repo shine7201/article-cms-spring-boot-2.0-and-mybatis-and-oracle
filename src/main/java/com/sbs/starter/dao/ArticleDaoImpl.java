7ac38cef0ce99d5af754a3dca7c48deba35a6b77
shine7201 <shine7201@gmail.com>
2020-04-18 12:46:27 +0900

    ArticleDao 생성 및 적용

---
+++ src/main/java/com/sbs/starter/dao/ArticleDaoImpl.java
@@ -0,0 +1,29 @@
+package com.sbs.starter.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.sbs.starter.dto.Article;
+
+@Component // 이렇게 해야 AutoWired 에 의한 연결에 적용된다.
+public class ArticleDaoImpl implements ArticleDao {
+
+	@Override
+	public List<Article> getList() {
+		//이건 가짜 데이터. 중간 확인용
+		Article article1 = new Article(1, "2019-08-20 12:12:12", "제목1", "내용1");
+		Article article2 = new Article(1, "2019-08-20 12:12:13", "제목2", "내용2");
+		Article article3 = new Article(1, "2019-08-20 12:12:14", "제목3", "내용3");
+		
+		List<Article> list = new ArrayList<Article>();
+		
+		list.add(article1);
+		list.add(article2);
+		list.add(article3);
+		
+		return list;
+	}
+
+}
--- src/main/webapp/WEB-INF/jsp/article/list.jsp
+++ src/main/webapp/WEB-INF/jsp/article/list.jsp
@@ -1,6 +1,4 @@
 <%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR"%>
-<%@ page import="java.util.*" %>
-<%@ page import="com.sbs.starter.dto.Article" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
@@ -12,7 +10,6 @@
 	<h1>게시물 리스트</h1>
 	
 		<h2>v2</h2>
-		
 		<!-- el의 장점은 request.getAttribute("article") 할 필요 없음. -->
 		<c:forEach items="${list}" var="article">
 			<section>
@@ -20,6 +17,5 @@
 			</section>
 			<hr>
 		</c:forEach>
-		
 	</body>
 </html>
